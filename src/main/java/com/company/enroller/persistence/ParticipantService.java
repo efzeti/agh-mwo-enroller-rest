b034505068e43170ad7a6d76dfd807eb7d6b9a74
efzeti <00franio@gmail.com>
2021-04-24 15:08:21 +0200

    Initial commit by me.

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -1,22 +1,72 @@
 package com.company.enroller.persistence;
 
-import java.util.Collection;
-
+import com.company.enroller.model.Participant;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
-import com.company.enroller.model.Participant;
+import java.util.Collection;
 
 @Component("participantService")
 public class ParticipantService {
 
-	DatabaseConnector connector;
+//	DatabaseConnector connector;
+	Session session;
 
 	public ParticipantService() {
-		connector = DatabaseConnector.getInstance();
+		session = DatabaseConnector.getInstance().getSession();
 	}
 
 	public Collection<Participant> getAll() {
-		return connector.getSession().createCriteria(Participant.class).list();
+		return session.createCriteria(Participant.class).list();
 	}
 
+    public Participant findByLogin(String login) {
+
+		return (Participant) DatabaseConnector.getInstance().getSession().get(Participant.class, login);
+
+
+//		Collection<Participant> participants = getAll();
+//
+//		Optional<Participant> participant = participants.stream().filter(p -> p.getLogin().equals(login)).findAny();
+//
+//		if (participant.isPresent()){
+//			return participant.get();
+//		} else {
+//			throw new IllegalStateException();
+//		}
+    }
+
+	public void addParticipant(Participant participant) {
+
+		Transaction transaction = this.session.beginTransaction();
+		session.save(participant);
+		transaction.commit();
+
+	}
+
+	public void deleteParticipant(Participant participant) { ;
+
+		Transaction transaction = this.session.beginTransaction();
+		session.delete(participant);
+		transaction.commit();
+	}
+
+//	public void putParticipant(String login, String password) { ;
+//
+//		Transaction transaction = this.session.beginTransaction();
+//
+//		Participant participant = findByLogin(login);
+//		participant.setPassword(password);
+//
+//		session.save(participant);
+//		transaction.commit();
+//	}
+
+	public void putParticipant(Participant participant) {
+
+		Transaction transaction = this.session.beginTransaction();
+		session.merge(participant);
+		transaction.commit();
+	}
 }
