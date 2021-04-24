a8e2b95e3fb7b18318ef06dab759c29033f0ea72
efzeti <00franio@gmail.com>
2021-04-24 16:04:11 +0200

    implementation of get and post meeting.

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,26 +1,41 @@
 package com.company.enroller.persistence;
 
-import java.util.Collection;
-
+import com.company.enroller.model.Meeting;
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
-import com.company.enroller.model.Meeting;
+import java.util.Collection;
 
 @Component("meetingService")
 public class MeetingService {
 
-	DatabaseConnector connector;
+//	DatabaseConnector connector;
+	Session session;
 
 	public MeetingService() {
-		connector = DatabaseConnector.getInstance();
+//		connector = DatabaseConnector.getInstance();
+		session = DatabaseConnector.getInstance().getSession();
 	}
 
 	public Collection<Meeting> getAll() {
 		String hql = "FROM Meeting";
-		Query query = connector.getSession().createQuery(hql);
+		Query query = session.createQuery(hql);
 		return query.list();
 	}
 
+	public Meeting findById(long id) {
+
+		return (Meeting) DatabaseConnector.getInstance().getSession().get(Meeting.class, id);
+
+	}
+
+	public void addMeeting(Meeting meeting) {
+
+		Transaction transaction = this.session.beginTransaction();
+		session.save(meeting);
+		transaction.commit();
+
+	}
 }
